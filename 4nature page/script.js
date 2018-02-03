5efa47c051af701cefa863ab358cb674b9444bd8
OlyaKN <34573390+OlyaKN@users.noreply.github.com>
2018-02-03 22:24:58 +0300

    34

--- 4nature page/script.js
+++ 4nature page/script.js
@@ -6,3 +6,16 @@ $( function(){
  });
 }
 );
+
+$(window).scroll(function(){
+ $('section').each(function(){ 
+   if ($(this).offset().top < $(window).scrollTop()) {
+     var difference = $(window).scrollTop() - $(this).offset().top;
+      var half = (difference / 2) + 'px',
+      transform = 'translate3d( 0, ' + half + ',0)';
+ $(this).find('img').css('transform', transform);
+ } else {
+ $(this).find('img').css('transform', 'translate3d(0,0,0)');
+		    }
+		  });
+});
